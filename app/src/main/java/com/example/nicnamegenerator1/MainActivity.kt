4117c19e730da9f22f695944de3c5fd11ac69322
habilithm97 <habinkim97@gmail.com>
2023-08-08 12:16:10 +0900

    메인 UI

--- app/src/main/java/com/example/nicnamegenerator1/MainActivity.kt
+++ app/src/main/java/com/example/nicnamegenerator1/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.nicnamegenerator1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nicnamegenerator1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
     }
 }
